10328c6a2a505a8e0027c971a6dea4ec952a3809
a.postnov <alex.postnov@corp.badoo.com>
2017-06-05 17:46:01 +0100

    add waits

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -28,31 +29,37 @@ public class FirstTest {
     @Before
     public void setUp() {
 
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 30);
+        wait = new WebDriverWait(driver, 1);
     }
 
     @Test
     public void testAdminLogin() {
         login();
 
-        int primaryLinksNum = driver.findElements(By.cssSelector("ul#box-apps-menu li")).size();
-        System.out.println("Primary: " + primaryLinksNum);
+        int primaryLinksNumber = driver.findElements(By.cssSelector("ul#box-apps-menu li")).size();
+        System.out.println("Primary: " + primaryLinksNumber);
 
-        for (int i = 1; i <= primaryLinksNum; i++) {
-            WebElement primaryLink = driver.findElement(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ")"));
+        for (int i = 1; i <= primaryLinksNumber; i++) {
+            WebElement primaryLink = driver.findElement(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ") a"));
             System.out.println(primaryLink.getText());
             primaryLink.click();
 
-            int secondaryLinks = driver.findElements(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ")")).size();
+            if (isElementPresent(By.cssSelector(".docs"))) {
+                int secondaryLinksNumber = driver.findElements(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ") a ")).size();
+                System.out.println(secondaryLinksNumber);
 
-            for (int j = 2; j < secondaryLinks; j++) {
-                WebElement secondaryLink = driver.findElement(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ") li:nth-of-type(" + j + ")"));
-                System.out.println("Secondary: " + secondaryLink.getText());
-                secondaryLink.click();
+                for (int j = 2; j < secondaryLinksNumber; j++) {
+                    WebElement secondaryLink = driver.findElement(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ") li:nth-of-type(" + j + ") a"));
+                    System.out.println("Secondary: " + secondaryLink.getText());
+                    wait.until(ExpectedConditions.visibilityOf(secondaryLink));
+                    secondaryLink.click();
+                    wait.until(ExpectedConditions.attributeToBe(By.cssSelector("#box-apps-menu li:nth-of-type(" + i + ") li:nth-of-type(" + j + ")"),"class", "selected"));
+                }
             }
+
+            Assert.assertTrue(isElementPresent(By.cssSelector("h1")));
             driver.get(BASE_URL);
         }
     }
@@ -72,6 +79,15 @@ public class FirstTest {
         wait.until(titleIs("My Store"));
 
         Assert.assertEquals("Wrong title","My Store", driver.getTitle());
+    }
+
+    private boolean isElementPresent (By locator) {
+        try {
+            wait.until((WebDriver d) -> (d.findElement(locator)));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
     }
 
     @After
