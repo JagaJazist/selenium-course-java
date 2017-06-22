5d90cb9efa9c89d51177bdd605723a4f6c5ef61e
a.postnov <alex.postnov@corp.badoo.com>
2017-06-22 17:43:42 +0100

    first task

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,39 +1,27 @@
 import Pages.LoginToAdminPage;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import java.util.List;
-import java.util.Set;
+import org.openqa.selenium.By;
 
 
 public class FirstTest extends TestBase {
 
-    public static final String BASE_URL = "http://localhost:8899/public_html/admin/?app=countries&doc=countries";
+    public static final String BASE_URL = "http://localhost:8899/public_html/admin/?app=catalog&doc=catalog&category_id=1:";
 
     @Test
-    public void testNewWindows() {
+    public void testBrowserLogs() {
         LoginToAdminPage loginToAdminPage = new LoginToAdminPage(driver);
         loginToAdminPage.open();
         loginToAdminPage.loginAsAdmin();
 
         driver.get(BASE_URL);
-        driver.findElement(By.cssSelector(".fa.fa-plus-circle")).click();
 
-        List<WebElement> links = driver.findElements(By.xpath("//*[contains(@class, 'fa-external-link')]/.."));
+        int productsNum = driver.findElements(By.xpath("//*[contains(@class, 'dataTable')]//img/following-sibling::a")).size();
 
-        for (WebElement link : links) {
-            Assert.assertEquals("_blank", link.getAttribute("target"));
-
-            String firstWindowHandle = driver.getWindowHandle();
-            link.click();
-            wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-            Set<String> allWindowHandles = driver.getWindowHandles();
-            allWindowHandles.remove(firstWindowHandle);
-            driver.switchTo().window((String) allWindowHandles.toArray()[0]);
-            driver.close();
-            driver.switchTo().window(firstWindowHandle);
+        for (int i = 0; i < productsNum; i++) {
+            driver.findElements(By.xpath("//*[contains(@class, 'dataTable')]//img/following-sibling::a")).get(i).click();
+            Assert.assertTrue(driver.manage().logs().get("browser").getAll().isEmpty());
+            driver.get(BASE_URL);
         }
     }
 }
